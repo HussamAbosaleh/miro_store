c5c167b62f8af3684b89de3a4c48e8cf5fd2adef
HussamAbosaleh <hussamabosaleh@gmail.com>
2026-02-22 21:51:05 +0100

    add admin authorization middleware

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,15 +2,22 @@ const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/user.controller");
 const { protect } = require("../middleware/auth.middleware");
+const { adminOnly } = require("../middleware/admin.middleware");
 
 
 
 
 
 router.post("/register", registerUser);
+
 router.post("/login", loginUser);
+
 router.get("/profile", protect, async (req, res) => {
   res.json(req.user);
+});
+
+router.get("/admin-test", protect, adminOnly, (req, res) => {
+  res.json({ message: "Welcome Admin" });
 });
 
 
