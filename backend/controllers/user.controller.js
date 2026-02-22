661f4c92a9d3e9b91bedc51af2d978baebcc8e52
HussamAbosaleh <hussamabosaleh@gmail.com>
2026-02-22 02:06:49 +0100

    remove env file from tracking

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-
+const jwt = require("jsonwebtoken");
 // ================= REGISTER =================
 const registerUser = async (req, res) => {
   try {
@@ -45,32 +45,37 @@ const registerUser = async (req, res) => {
 };
 
 // ================= LOGIN =================
+// ================= LOGIN =================
 const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Validation
     if (!email || !password) {
       return res.status(400).json({ message: "Email and password are required" });
     }
 
-    //password 
     const user = await User.findOne({ email }).select("+password");
 
     if (!user) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    // Compare password
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    // Clean response
+    //  إنشاء التوكن
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
     res.status(200).json({
       message: "Login successful",
+      token,
       user: {
         _id: user._id,
         name: user.name,
@@ -80,6 +85,7 @@ const loginUser = async (req, res) => {
     });
 
   } catch (error) {
+     console.log(error); //
     res.status(500).json({ message: "Server error" });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/user.routes");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const PORT = process.env.PORT;
