f814fc6fb0b8d9d14a590e8e3479e4ff536f4283
HussamAbosaleh <hussamabosaleh@gmail.com>
2026-02-22 01:08:46 +0100

    add login functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,38 +1,67 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+// ================= REGISTER =================
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    // تحقق
     if (!name || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    // تحقق إذا الإيميل موجود
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "Email already exists" });
     }
-    // تشفير الباسورد
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // إنشاء المستخدم
     const user = await User.create({
-    name,
-    email,
-    password: hashedPassword,
-   });
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     res.status(201).json({
       message: "User registered successfully",
       user,
     });
+
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
 };
 
-module.exports = { registerUser };
+// ================= LOGIN =================
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      user,
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { registerUser, loginUser };
