52c5eb1bcbe3755412705b6716c147cea003819b
HussamAbosaleh <hussamabosaleh@gmail.com>
2026-02-22 01:15:44 +0100

    remove password from login response

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -55,9 +55,14 @@ const loginUser = async (req, res) => {
     }
 
     res.status(200).json({
-      message: "Login successful",
-      user,
-    });
+  message: "Login successful",
+  user: {
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  },
+  });
 
   } catch (error) {
     res.status(500).json({ message: "Server error" });
